e18ea266adc2af43349b234d670db9cc39b9056b
Mauricio Carvalho <mauriciocarvalhoac@gmail.com>
2026-08-17 22:58:31 -0300

    Ajuste da inicializacao do projeto

--- src/main/java/com/mrc/clinic/clinic_api/ClinicApiApplication.java
+++ src/main/java/com/mrc/clinic/clinic_api/ClinicApiApplication.java
@@ -3,12 +3,17 @@ package com.mrc.clinic.clinic_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @SpringBootApplication
 public class ClinicApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicApiApplication.class, args);
-        System.out.println("Aplicação iniciada com sucesso!!");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String date = sdf.format(new Date());
+        System.out.println("Aplicação iniciada com sucesso às " + date);
     }
 
 }
